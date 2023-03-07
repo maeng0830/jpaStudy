27d8a8ee1e456c7912748d2e1f96ed44ddeaa8b9
maeng0830 <kmk930830@gmail.com>
2023-03-07 22:18:01 +0900

    Feat: 데이터 중심 설계
    - 테이블의 외래키를 객체에 그대로 사용
    - 객체 그래프 탐색이 불가능
    - 엔티티 연관관계 매핑을 통해 객체 중심 설계 가능

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,63 @@
+package jpabook.jpashop.domain;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Orders")
+public class Order {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "order_id")
+	private Long id;
+
+	@Column(name = "member_id")
+	private Long memberId;
+
+	// 현재는 ORDERDATE로 DB에 저장된다.
+	// 스프링 부트를 사용할 경우, 관례를 자동으로 적용해준다. 즉 DB에 order_date로 저장된다.
+	private LocalDateTime orderDate;
+
+	@Enumerated(EnumType.STRING)
+	private OrderStatus status;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getMemberId() {
+		return memberId;
+	}
+
+	public void setMemberId(Long memberId) {
+		this.memberId = memberId;
+	}
+
+	public LocalDateTime getOrderDate() {
+		return orderDate;
+	}
+
+	public void setOrderDate(LocalDateTime orderDate) {
+		this.orderDate = orderDate;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+}
