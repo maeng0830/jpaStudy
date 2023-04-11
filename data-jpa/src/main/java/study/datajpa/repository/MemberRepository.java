9e173ba86f62a947d39fcd73e7544653e9d0f8ed
maeng0830 <kmk930830@gmail.com>
2023-04-11 18:53:37 +0900

    Feat: 쿼리 메소드
    - 메소드명 사용
    - @Query 사용

--- data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,8 +1,29 @@
 package study.datajpa.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+	List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+	List<Member> findTop3By();
+
+	// 메소드명 간략화, 리포지토리 메소드에 바로 JPQL 작성 가능
+	// 쿼리에 문법 오류가 있을 경우, 컴파일 예외 발생
+	@Query("select m from Member m where m.username = :username and m.age = :age")
+	List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+	// String 타입으로 조회
+	@Query("select m.username from Member m")
+	List<String> findUsernameList();
+
+	// DTO 타입으로 조회
+	// DTO에 해당하는 생성자가 필요하다. MemberDto(m.id, m.username, t.name)
+	@Query("select new study.datajpa.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
+	List<MemberDto> findMemberDto();
 }
--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -2,11 +2,14 @@ package study.datajpa.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
+import study.datajpa.entity.Team;
 
 @SpringBootTest
 @Transactional
@@ -14,9 +17,13 @@ class MemberRepositoryTest {
 
 	@Autowired
 	MemberRepository memberRepository;
+	@Autowired
+	TeamRepository teamRepository;
 
 	@Test
 	public void testMember() {
+		System.out.println("memberRepository.getClass() = " + memberRepository.getClass());
+
 		Member member = new Member("memberA");
 		Member savedMember = memberRepository.save(member);
 
@@ -24,5 +31,93 @@ class MemberRepositoryTest {
 		assertThat(findMember.getId()).isEqualTo(member.getId());
 		assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
 		assertThat(findMember).isEqualTo(member);
+	}
+
+	@Test
+	public void basicCRUD() {
+		Member member1 = new Member("member1");
+		Member member2 = new Member("member2");
+		memberRepository.save(member1);
+		memberRepository.save(member2);
+
+		member1.setUsername("member111");
+
+		// 단건 조회 검증
+		Member findMember1 = memberRepository.findById(member1.getId()).get();
+		Member findMember2 = memberRepository.findById(member2.getId()).get();
+		assertThat(findMember1).isEqualTo(member1);
+		assertThat(findMember2).isEqualTo(member2);
+
+		// 데이터 변경 검증
+		assertThat(findMember1.getUsername()).isEqualTo("member111");
+
+
+		//리스트 조회 검증
+		List<Member> all = memberRepository.findAll();
+		assertThat(all.size()).isEqualTo(2);
+
+		//카운트 검증
+		long count = memberRepository.count();
+		assertThat(count).isEqualTo(2);
+
+		//삭제 검증
+		memberRepository.delete(member1);
+		memberRepository.delete(member2);
+		long deleteCount = memberRepository.count();
+		assertThat(deleteCount).isEqualTo(0);
+	}
+
+	@Test
+	public void findByUsernameAndGreaterThan() {
+		Member m1 = new Member("AAA", 10);
+		Member m2 = new Member("AAA", 20);
+		memberRepository.save(m1);
+		memberRepository.save(m2);
+
+		List<Member> result = memberRepository.findByUsernameAndAgeGreaterThan("AAA", 15);
+		assertThat(result.size()).isEqualTo(1);
+		assertThat(result.get(0).getAge()).isEqualTo(20);
+	}
+
+	@Test
+	public void findHelloBy() {
+		List<Member> helloBy = memberRepository.findTop3By();
+	}
+
+	@Test
+	public void testQuery() {
+		Member m1 = new Member("AAA", 10);
+		Member m2 = new Member("AAA", 20);
+		memberRepository.save(m1);
+		memberRepository.save(m2);
+
+		List<Member> result = memberRepository.findUser("AAA", 10);
+		assertThat(result.get(0)).isEqualTo(m1);
+	}
+
+	@Test
+	public void findUsernameList() {
+		Member m1 = new Member("AAA", 10);
+		Member m2 = new Member("BBB", 20);
+		memberRepository.save(m1);
+		memberRepository.save(m2);
+
+		List<String> result = memberRepository.findUsernameList();
+		assertThat(result.size()).isEqualTo(2);
+	}
+
+	@Test
+	public void findMemberDto() {
+		Team team = new Team("teamA");
+		teamRepository.save(team);
+
+		Member m1 = new Member("AAA", 10);
+		memberRepository.save(m1);
+		m1.changeTeam(team);
+
+		List<MemberDto> result = memberRepository.findMemberDto();
+		assertThat(result.get(0)).isInstanceOf(MemberDto.class);
+
+		System.out.println("result = " + result.get(0));
 	}
 }
