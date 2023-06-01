62e067356d789dc93a873cc7c5c8a77843517dd0
maeng0830 <kmk930830@gmail.com>
2023-06-01 16:23:58 +0900

    Feat: 순수 JPA & Querydsl

---
+++ querydsl/src/main/java/study/querydsl/repository/MemberJpaRepository.java
@@ -0,0 +1,141 @@
+package study.querydsl.repository;
+
+import static org.springframework.util.StringUtils.*;
+import static study.querydsl.entity.QMember.*;
+import static study.querydsl.entity.QTeam.*;
+
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
+import java.util.Optional;
+import javax.persistence.EntityManager;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
+import study.querydsl.dto.QMemberDto;
+import study.querydsl.dto.QMemberTeamDto;
+import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
+import study.querydsl.entity.QTeam;
+
+@Repository
+public class MemberJpaRepository {
+
+	private final EntityManager em; // 스프링이 스레드 별로 작동하도록 처리해준다 -> 동시성 문제 해결
+	private final JPAQueryFactory queryFactory;
+
+	public MemberJpaRepository(EntityManager em, JPAQueryFactory queryFactory) {
+		this.em = em;
+		this.queryFactory = queryFactory;
+	}
+
+	public void save(Member member) {
+		em.persist(member);
+	}
+
+	public Optional<Member> findById(Long id) {
+		Member findMember = em.find(Member.class, id);
+		return Optional.ofNullable(findMember);
+	}
+
+	public List<Member> findAll() {
+		return em.createQuery("select m from Member m", Member.class)
+				.getResultList();
+	}
+
+	public List<Member> findAll_querydsl() {
+		return queryFactory
+				.selectFrom(member)
+				.fetch();
+	}
+
+	public List<Member> findByUsername(String username) {
+		return em.createQuery("select m from Member m where m.username = :username", Member.class)
+				.setParameter("username", username)
+				.getResultList();
+	}
+
+	public List<Member> findByUsername_querydsl(String username) {
+		return queryFactory
+				.selectFrom(member)
+				.where(member.username.eq(username))
+				.fetch();
+	}
+
+	public List<MemberTeamDto> searchByBuilder(MemberSearchCondition condition) {
+		BooleanBuilder builder = new BooleanBuilder();
+		if (hasText(condition.getUsername())) { // null, "" 둘다 처리
+			builder.and(member.username.eq(condition.getUsername()));
+		}
+
+		if (hasText(condition.getTeamName())) {
+			builder.and(team.name.eq(condition.getTeamName()));
+		}
+
+		if (condition.getAgeGoe() != null) {
+			builder.and(member.age.goe(condition.getAgeGoe()));
+		}
+
+		if (condition.getAgeLoe() != null) {
+			builder.and(member.age.goe(condition.getAgeLoe()));
+		}
+
+		return queryFactory
+				.select(new QMemberTeamDto(
+						member.id.as("memberId"),
+						member.username,
+						member.age,
+						team.id.as("teamId"),
+						team.name.as("teamName")
+				))
+				.from(member)
+				.where(builder)
+				.leftJoin(member.team, team)
+				.fetch();
+	}
+
+	public List<MemberTeamDto> search(MemberSearchCondition condition) {
+
+
+		return queryFactory
+				.select(new QMemberTeamDto(
+						member.id.as("memberId"),
+						member.username,
+						member.age,
+						team.id.as("teamId"),
+						team.name.as("teamName")
+				))
+				.from(member)
+				.where(
+						usernameEq(condition.getUsername()),
+						teamNameEq(condition.getTeamName()),
+						ageGoe(condition.getAgeGoe()),
+						ageLoe(condition.getAgeLoe())
+				)
+				.leftJoin(member.team, team)
+				.fetch();
+	}
+
+	private BooleanExpression usernameEq(String username) {
+		return hasText(username) ? member.username.eq(username) : null;
+	}
+
+	private BooleanExpression teamNameEq(String teamName) {
+		return hasText(teamName) ? team.name.eq(teamName) : null;
+	}
+
+	private BooleanExpression ageGoe(Integer ageGoe) {
+		return ageGoe != null ? member.age.goe(ageGoe) : null;
+	}
+
+	private BooleanExpression ageLoe(Integer ageLoe) {
+		return ageLoe != null ? member.age.loe(ageLoe) : null;
+	}
+
+	private BooleanExpression ageBetween(Integer ageGoe, Integer ageLoe) {
+		return ageGoe(ageGoe).and(ageLoe(ageLoe));
+	}
+}
