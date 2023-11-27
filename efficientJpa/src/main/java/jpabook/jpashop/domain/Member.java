5370f014309cac3f6a9ace3a6ef3c6738c898806
maeng0830 <kmk930830@gmail.com>
2023-11-27 22:35:35 +0900

    Feat: API 개발   - 회원 등록 API, 회원 수정 API, 회원 조회 API 개발   - API 요청 및 응답 데이터로 엔티티를 사용하지 말자. 별도의 DTO를 생성하여 사용하자.

--- efficientJpa/src/main/java/jpabook/jpashop/domain/Member.java
+++ efficientJpa/src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,12 +1,16 @@
 package jpabook.jpashop.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.Getter;
-import lombok.Setter;
-
-import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Getter @Setter
